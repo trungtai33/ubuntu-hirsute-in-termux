0dc4ccb2be18a85ec9592094b125b5139c199b76
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-09-23 18:56:13 +0700

    official

--- uninstall/ubuntu-hirsute.sh
+++ uninstall/ubuntu-hirsute.sh
@@ -3,7 +3,7 @@ if [ -d "$PREFIX/share/ubuntu-hirsute" ]; then
 printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling Ubuntu Hirsute, please wait...\n\e[31m"
 rm -rf "$PREFIX/share/ubuntu-hirsute"
 rm -f "$PREFIX/bin/start-ubuntu-hirsute"
-printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstalled successfully.\n\n\e[0m"
+printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall successfully.\n\n\e[0m"
 exit
 fi
 printf "\n\e[31mError: distribution Ubuntu Hirsute is not installed.\n\n\e[0m"
