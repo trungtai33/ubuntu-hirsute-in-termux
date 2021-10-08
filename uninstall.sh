c471ae75bd4ab8398d8a8d0f618b8b0911a89050
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-10-08 12:55:58 +0700

    official

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,7 @@
 directory="ubuntu-hirsute"
 distro_name="Ubuntu Hirsute"
 if [ -d "${PREFIX}/share/${directory}" ]; then
-printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distro_name}, please wait...\n\e[31m"
+printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distro_name}, please wait...\n\e[0m"
 rm -rf "${PREFIX}/share/${directory}"
 rm -f "${PREFIX}/bin/start-${directory}"
 printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall successfully.\n\n\e[0m"
