e6986d202bc1007a75a6a8bf16504f04eef5624f
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-10-08 12:56:54 +0700

    official

--- install.sh
+++ install.sh
@@ -21,12 +21,16 @@ apt update > /dev/null 2>&1
 apt install -y proot > /dev/null 2>&1
 tarball="rootfs.tar.gz"
 printf "\e[34m[\e[32m*\e[34m]\e[36m Downloading ${distro_name}, please wait...\n\n\e[34m"
-curl --fail --retry 5 --location --output "${tarball}" \
-"https://partner-images.canonical.com/core/hirsute/current/ubuntu-hirsute-core-cloudimg-${arch}-root.tar.gz"
+if ! curl --fail --retry 5 --location --output "${tarball}" \
+"https://partner-images.canonical.com/core/hirsute/current/ubuntu-hirsute-core-cloudimg-${arch}-root.tar.gz"; then
+printf "\n\e[34m[\e[32m*\e[34m]\e[36m Download failure, please check your network connection.\n\n\e[0m"
+rm -f "${tarball}"
+exit
+fi
 mkdir -p "${PREFIX}/share/${directory}"
-printf "\n\e[34m[\e[32m*\e[34m]\e[36m Installing ${distro_name}, please wait...\n\e[31m"
+printf "\n\e[34m[\e[32m*\e[34m]\e[36m Installing ${distro_name}, please wait...\n\e[0m"
 proot --link2symlink tar -xf "${tarball}" --directory="${PREFIX}/share/${directory}" --exclude='dev'||:
-printf "\e[34m[\e[32m*\e[34m]\e[36m Setting up ${distro_name}, please wait...\n\e[31m"
+printf "\e[34m[\e[32m*\e[34m]\e[36m Setting up ${distro_name}, please wait...\n\e[0m"
 rm -f "${tarball}"
 cat <<- EOF > "${PREFIX}/share/${directory}/etc/ld.so.preload"
 /lib/${platform}/libgcc_s.so.1
