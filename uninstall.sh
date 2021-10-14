245f6926795ebd98d1a27ff02e10e7cdc9da3e8a
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-10-14 15:59:47 +0700

    official

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/bash
 directory="ubuntu-hirsute"
-distro_name="Ubuntu Hirsute"
+distribution="Ubuntu Hirsute"
 if [ -d "${PREFIX}/share/${directory}" ]; then
-printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distro_name}, please wait...\n\e[0m"
+printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distribution}, please wait...\n\e[0m"
 rm -rf "${PREFIX}/share/${directory}"
 rm -f "${PREFIX}/bin/start-${directory}"
 printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall successfully.\n\n\e[0m"
 exit
 fi
-printf "\n\e[31mError: distribution ${distro_name} is not installed.\n\n\e[0m"
+printf "\n\e[31mError: distribution ${distribution} is not installed.\n\n\e[0m"
