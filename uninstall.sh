c260405a08d26c89a31f4675b4c61897a737b361
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-11-12 13:10:14 +0700

    official

--- uninstall.sh
+++ uninstall.sh
@@ -2,10 +2,10 @@
 directory="ubuntu-hirsute"
 distribution="Ubuntu Hirsute"
 if [ ! -d "${PREFIX}/share/${directory}" ]; then
-printf "\n\e[31mError: distribution ${distribution} is not installed.\n\n\e[0m"
+printf "\n\e[31mError: distribution ${distribution} is not installed.\e[0m\n\n"
 exit
 fi
-printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distribution}, please wait...\n\e[0m"
+printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distribution}, please wait...\e[0m\n"
 rm -rf "${PREFIX}/share/${directory}"
 rm -f "${PREFIX}/bin/start-${directory}"
-printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall finished.\n\n\e[0m"
+printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall finished.\e[0m\n\n"
